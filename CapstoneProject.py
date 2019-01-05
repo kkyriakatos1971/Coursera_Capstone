117efbb7d418f61c4a392db534f5dbedacc698f2
kkyriakatos1971 <46407080+kkyriakatos1971@users.noreply.github.com>
2019-01-05 22:45:58 +0100

    Add files via upload

---
+++ CapstoneProject.py
@@ -0,0 +1,22 @@
+
+# coding: utf-8
+
+# This is the main notebook for the Capstone Project
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[3]:
+
+
+print("Hello Capstone Project Course!")
+
